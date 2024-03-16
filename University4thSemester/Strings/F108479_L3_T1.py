a3944c053f0aad255622d5af17d777d1c15fe579
Yane Yanev <93374652+Yaneww11@users.noreply.github.com>
2024-03-16 21:01:33 +0200

    Update F108479_L3_T1.py

--- University4thSemester/Strings/F108479_L3_T1.py
+++ University4thSemester/Strings/F108479_L3_T1.py
@@ -2,18 +2,18 @@ import sys
 
 input = sys.argv[1:]
 text = input[0].strip()
-step = int(input[1].strip())
+key = int(input[1].strip())
 cipher_text = ""
 
 for ch in text:
-    shiffer_character_code = ord(ch) + step
+    shiffer_character_code = ord(ch) + key
     if ch.isupper():
         if shiffer_character_code > 90:
-            shiffer_character_code = (shiffer_character_code % 90) + 65
+            shiffer_character_code = (shiffer_character_code % 90) + 64
         cipher_text += chr(shiffer_character_code)
-    if ch.lower():
+    if ch.islower():
         if shiffer_character_code > 122:
-            shiffer_character_code = (shiffer_character_code % 122) + 97
+            shiffer_character_code = (shiffer_character_code % 122) + 96
         cipher_text += chr(shiffer_character_code)
 
 print(cipher_text)
