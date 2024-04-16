3b9f81899ab0a62326e4afffe58bb94dfab786a2
Yane Yanev <93374652+Yaneww11@users.noreply.github.com>
2024-04-16 12:55:43 +0300

    Update DFSGraph.py

--- Algorithm4thSemester/Exersise/graphs/DFSGraph.py
+++ Algorithm4thSemester/Exersise/graphs/DFSGraph.py
@@ -10,22 +10,42 @@ def ReadGraph(inputData, n):
     return vertices
 
 
-def DFS(vertex, vertices, used):
-    if used[vertex]:
-        return
+def DFS(vertex, vertices, used, path, prefix):
     used[vertex] = True
-    print(vertex + 1)
-    for nextVertex in vertices[vertex]:
-        DFS(nextVertex, vertices, used)
+    print(f"{prefix} {vertex + 1}")
+    for nextVertex in sorted(vertices[vertex]):
+        if used[nextVertex]:
+            continue
+        path[nextVertex] = vertex
+        DFS(nextVertex, vertices, used, path, prefix + "  ")
 
 
 def printPathWithDFS(vertices):
-    vertex = 0
+    # Root of the result(tree)
+    vertex = 2 - 1
+    # List with element, if in index is true(that element is visited)
     used = [False] * n
-    DFS(vertex, vertices, used)
+    # Path for the number
+    path = [-1] * n
+    # The path start from vertex
+    path[vertex] = -1
+    DFS(vertex, vertices, used, path, "")
+    # Print path for all
+    for i in range(len(vertices)):
+        print(f"{path[i] + 1} -> {i + 1}")
+
+    # Print path from the root to number
+    current = 4 - 1
+    pathToCurrentNum = []
+    while current != -1:
+        pathToCurrentNum.append(current + 1)
+        current = path[current]
+
+    pathToCurrentNum.reverse()
+    print(f"Path to {pathToCurrentNum[-1]}: {' -> '.join([str(i) for i in pathToCurrentNum])}")
 
 
-inputData = [(2, 6), (1, 4), (1, 7), (2, 7),(3, 6), (3, 5), (5, 6), (3, 7)]
+inputData = [(2, 6), (2, 7), (1, 4), (1, 7), (3, 6), (3, 5), (5, 6), (3, 7), ]
 n = len(inputData)
 
 vertices = ReadGraph(inputData, n)
